36dacf932c7dc41c7313af6283b3e4db19cfa24e
marciovz <marciovz@yahoo.com.br>
2023-01-30 20:58:54 -0300

    Configurando fontes externas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1
-      className={styles.title}>hello World
-    </h1>
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1
+        className={styles.title}>hello World
+      </h1>
+    </>
   )
 }
