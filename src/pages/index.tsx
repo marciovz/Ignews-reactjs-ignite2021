9491ca6e1dc3a0f787e8b8db19d8fd00566f833e
marciovz <marciovz@yahoo.com.br>
2023-01-31 00:20:39 -0300

    Configurando o stripe e consumindo a api com SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,13 +27,37 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            { product.amount && <span>for {product.amount} month</span> }
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={ product.priceId }/>
         </section>
 
         <Image src="/images/avatar.svg" alt="Girl coding" width={334} height={520} />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JuM3OCQfvCbujiH5HLLgIu3',{
+    expand: ['product']
+  });
+
+  const amountFormattedOrNull = price.unit_amount
+    ? new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount! / 100)
+    : null;
+  
+  const product = {
+    priceId: price.id,
+    amount: amountFormattedOrNull
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
---
+++ src/services/stripe.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe';
+//import { version } from '../../package.json'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY as string,
+  {
+    apiVersion: '2022-11-15',
+    appInfo: {
+      name: 'MvzIgNews',
+      version: '0.0.1'
+    }
+  }
+)
