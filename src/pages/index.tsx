32cd1cad50048a67c2336e8cce00f4ef07af7b95
marciovz <marciovz@yahoo.com.br>
2023-01-30 23:25:32 -0300

    Implementado o componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -18,6 +20,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <Image src="/images/avatar.svg" alt="Girl coding" width={334} height={520} />
