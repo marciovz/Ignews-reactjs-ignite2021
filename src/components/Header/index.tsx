6bef5586e30de1829149953c859856c746b7b3de
marciovz <marciovz@yahoo.com.br>
2023-01-30 22:47:45 -0300

    Implementado componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+
+  return (
+    <header className={styles.headerContainer} >
+      <div className={styles.headerContent} >
+        <Image src="/images/logo.svg" alt="ig.news" width={108}  height={30} />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+
+  )
+}
