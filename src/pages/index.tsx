4417e00004e4666b9ae1904e2c40a71922d6eafa
marciovz <marciovz@yahoo.com.br>
2023-01-30 23:17:24 -0300

    implementado a página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -7,7 +10,18 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <h1>hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={334} height={520} />
+      </main>
     </>
   )
 }
