844a129e1ef809c7fd5b56110f420a0f1c3829cb
marciovz <marciovz@yahoo.com.br>
2023-02-06 15:22:35 -0300

    alterado o arquivo /testes/setupTests.js para jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/tests/setupTests.ts'
-  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
