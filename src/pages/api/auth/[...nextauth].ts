f4dff824cfb8ac5a5fc5acd19b38cc967253104f
marciovz <marciovz@yahoo.com.br>
2023-02-08 22:28:19 -0300

    adicionado a variável nextauth secret nas configurações da autenticação

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
